1a537557499f6f545a81e31271d4a890d9912702
rocketgithub <rodrigo@solucionesprisma.com>
2020-06-23 16:55:23 -0600

    Ya no es necesario indicar si es anulación

--- models/account.py
+++ models/account.py
@@ -278,7 +278,6 @@ class AccountInvoice(models.Model):
                     "archivo": xmls_base64.decode("utf-8"),
                     "codigo": factura.company_id.vat.replace('-',''),
                     "alias": factura.journal_id.usuario_fel,
-                    "es_anulacion": "N"
                 }
                 r = requests.post('https://signer-emisores.feel.com.gt/sign_solicitud_firmas/firma_xml', json=data, headers=headers)
                 logging.warn(r.text)
